81ef452642d35fe11b21df893706a82908f91dcc
BigRockerEnding <bigrockerending@live.com>
2015-12-16 18:31:35 -0800

    g

--- src/level4/Sorting.java
+++ src/level4/Sorting.java
@@ -7,11 +7,28 @@ public class Sorting {
 		String[] sortedArray = {};
 		ArrayList<String> strings = new ArrayList<String>();
 		for(int i = 0; i < testArray.length; i++) {
-			if(testArray[i].compareTo(testArray[i+1]) < 0) {
+			if(i == testArray.length-1) {
 				strings.add(testArray[i]);
 			}
+			else if(testArray[i].compareTo(testArray[i+1]) < 0) {
+				strings.add(testArray[i]);
+			}
+			
 		}
-		System.out.println(strings);
+		
+		for( int j = 0; j < testArray.length; j++) {
+			if(j == 0) {
+				sortedArray[j] = strings.get(j);
+			} else {
+				if(strings.get(j).compareTo(sortedArray[j-1]) < 0) {
+					
+				} else {
+					sortedArray[j] = strings.get(j);
+				}
+			}
+			
+		}
+		
 	}
 
 
