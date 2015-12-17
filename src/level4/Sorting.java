91cfd10d566dbb04de60eee9b7667d0dacde7611
BigRockerEnding <bigrockerending@live.com>
2015-12-16 18:31:07 -0800

    I made the same mistake as before, just more elaborately

--- src/level4/Sorting.java
+++ src/level4/Sorting.java
@@ -1,17 +1,19 @@
 package level4;
 
+import java.util.ArrayList;
+
 public class Sorting {
-
 	public static void sort(String[] testArray) {
-
-		for (int i = testArray.length; i > -1; i--) {
-			if (testArray[i - 1].compareTo(testArray[i]) > 0) { // thing before it comes after it in the alphabet
-				String holder = testArray[i];
-				testArray[i] = testArray[i - 1];
-				testArray[i - 1] = holder;
+		String[] sortedArray = {};
+		ArrayList<String> strings = new ArrayList<String>();
+		for(int i = 0; i < testArray.length; i++) {
+			if(testArray[i].compareTo(testArray[i+1]) < 0) {
+				strings.add(testArray[i]);
 			}
 		}
-
+		System.out.println(strings);
 	}
+
+
 
 }
